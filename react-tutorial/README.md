b1e4a26a9ada978cfdf0bde56874370e3a528d99
Luzy <felixxg@foxmail.com>
2020-02-27 10:53:12 +0800

    react-tutorial

---
+++ react-tutorial/README.md
@@ -0,0 +1,2 @@
+[文档](https://note.youdao.com/)</br>
+[tutorial](https://note.youdao.com/)
---
+++ react-tutorial/src/index.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import React, { Component } from 'react';
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+// class App extends Component {
+//     render(){
+//         return (
+//             <div className="App">
+//                 <h1>Hello,React!</h1>
+//             </div>
+//         )
+//     }
+// }
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
